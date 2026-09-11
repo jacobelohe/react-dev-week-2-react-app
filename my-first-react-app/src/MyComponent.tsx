b6dc602849c1c6c17f80505db89be912c36780a3
jacobelohe <j.ndayishim@slustudent.com>
2026-09-11 13:28:05 +0200

    Add styling to MyComponent

--- my-first-react-app/src/MyComponent.tsx
+++ my-first-react-app/src/MyComponent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './MyComponent.css';
 
 const MyComponent: React.FC = () => {
   return (
     <div>
-      <h1>Hello from MyComponent!</h1>
+      <h1>Hello from MyComponent! Elohe</h1>
       <p>This is a paragraph of text within my component.</p>
     </div>
   );
